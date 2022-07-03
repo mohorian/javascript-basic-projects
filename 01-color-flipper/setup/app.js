092ebcf60da758bf7736d79422a2e771613260fa
mohorian <mohorian1992@gmail.com>
2022-07-03 22:39:58 +0300

    js is done

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,14 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const button = document.getElementById('btn');
-const colorName = document.querySelector('.color');
+const color = document.querySelector('.color');
 
 function getRandomColor(array) {
     return array[Math.floor(Math.random() * array.length)]
 }
 
-button.addEventListener('click', function () {
+button.addEventListener('click', () => {
     const randomColor = getRandomColor(colors);
     document.body.style.backgroundColor = randomColor;
-    colorName.textContent = randomColor;
+    color.textContent = randomColor;
 })

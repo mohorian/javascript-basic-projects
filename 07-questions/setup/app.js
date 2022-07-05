93a2c7737c6c02953958989cca1b620a7e503f78
mohorian <mohorian1992@gmail.com>
2022-07-05 15:36:18 +0300

    js is done

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,17 @@
 //using selectors inside the element
 // traversing the dom
+
+const buttons = document.querySelectorAll('.question-btn');
+const articles = document.querySelectorAll('.question');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (evt) => {
+        const question = evt.currentTarget.parentElement.parentElement;
+        articles.forEach((article) => {
+            if (article !== question) {
+                article.classList.remove('show-text');
+            }
+        });
+        question.classList.toggle('show-text');
+    });
+});
